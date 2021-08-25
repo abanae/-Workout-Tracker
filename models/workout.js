8b7704a0cac02def7d27d28c4d426d12e94f3a48
Ana Ea <aabad0464@gmail.com>
2021-08-25 00:13:36 -0600

    created post route

--- models/workout.js
+++ models/workout.js
@@ -11,12 +11,15 @@ const workoutSchema = new Schema({
         {
          type: {
              type: String,
+             required: "Enter an exercise type",
          },
          name: {
              type: String,
+             required: "Enter an exercise name",
          },
          duration: {
              type: Number,
+             required: "Enter an exercise duration in minutes",
          },
          weight:{
              type: Number,
--- routes/api.js
+++ routes/api.js
@@ -2,15 +2,40 @@ const router = require('express').Router();
 const { Workout } = require('../models');
 
 
-
+// Getting Workouts
 router.get('/api/workouts/', async (req, res) => {
-    
     try {
         const allWorkouts = await Workout.find();
         res.json(allWorkouts);
-    }catch (err) {
-        res.json(err);
+    } catch (e) {
+        res.json(e);
+    }
+});
+
+// Add Exercise
+router.put('/api/workouts/:id', async (req, res) => {
+    try {
+        const addWorkout = await Workout.findByIdAndUpdate(req.params.id,
+            { $push: { exercises: body } },
+            { new: true }
+        )
+        res.json(addWorkout);
+    } catch (e) {
+        res.json(e);
+    }
+});
+
+// Create Workout
+router.post("/api/workouts", async (req, res) => {
+    try {
+        const createWorkout = await Workout.create();
+        res.json(createWorkout);
+    } catch (e) {
+        res.json(e);
     }
 });
 
 module.exports = router;
+
+
+

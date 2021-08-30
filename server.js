9e1fda2d0b2c283635d686dfcf04d157d26a16e6
Ana Ea <aabad0464@gmail.com>
2021-08-29 23:59:12 -0600

    deploying

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost:27017/workout', {
+mongoose.connect('mongodb://localhost:27017/workoutDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
